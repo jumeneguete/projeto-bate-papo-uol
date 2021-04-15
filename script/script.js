0e63afb6814b3a4f990c4c9e6a6a38d36fd46a41
Juliana Meneguete <jumeneguete@gmail.com>
2021-04-15 13:02:10 -0300

    carregando as mensagens do servido ao entrar na sala

--- script/script.js
+++ script/script.js
@@ -3,18 +3,19 @@ function entrarNaSala(){
     const nomeObj = {name:nome}
 
     const promessa = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants", nomeObj);
-    promessa.then(entradaParticipante);
+    promessa.then(entradaDoParticipante);
     promessa.catch(insiraOutroNome);
 }
 
-function entradaParticipante(resposta){
-    console.log(resposta)
+function entradaDoParticipante(resposta){
+    //console.log(resposta)
     const entrar = document.querySelector(".name-request");
     entrar.classList.add("hide");
 
     const salaDoChat = document.querySelector(".index");
     salaDoChat.classList.remove("hide");
 
+    carregarMensagens();
 }
 
 function insiraOutroNome(resposta){
@@ -24,4 +25,29 @@ function insiraOutroNome(resposta){
         const msgDeErro = document.querySelector(".name-request span");
         msgDeErro.classList.remove("hide");
     }
+}
+
+function carregarMensagens (){
+    const promessa = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages");
+    promessa.then(renderizarMensagens);
+}
+
+function renderizarMensagens (resposta){
+    let ulMensagens = document.querySelector(".all-messages");
+    ulMensagens.innerHTML = "";
+
+    console.log(resposta.data);
+    console.log(resposta.data[9].from);
+
+
+    for(let i=0; i < resposta.data.length; i++){
+        ulMensagens.innerHTML +=`
+            <li class="message"> 
+            ${resposta.data[i].time} ${resposta.data[i].from} para ${resposta.data[i].to} ${resposta.data[i].text}
+            </li>
+        `;
+    }
+
+    console.log(ulMensagens)
+
 }
